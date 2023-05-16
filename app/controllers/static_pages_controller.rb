55cd763356ec83e319f6b4e34c5ba438f8c0a760
hung <hunglt.ee@gmail.com>
2023-05-16 22:30:47 +0700

    more simple test

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,14 @@
 class StaticPagesController < ApplicationController
+  before_action :authenticate_user!
+
   def home
   end
+
+  def help
+  end
+
+  private
+    def authenticate_user!
+      redirect_to '/auth/login' unless current_user
+    end
 end
