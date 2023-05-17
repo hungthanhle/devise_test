2bcb9a687f6005673c6d163b0992525e58f9b938
hung <hunglt.ee@gmail.com>
2023-05-17 16:12:58 +0700

    view authorize

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,6 @@ class UsersController < ApplicationController
     if @user.nil?
       @user = {"email": "not found"}
     end
-    if current_user.can? :edit, @user
-      render json: {email: @user[:email]}
-    else
-      render html: "not permission"
-    end
   end
 
   def index
