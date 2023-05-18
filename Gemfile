7ef2cf958f3a6b4add56e6ad9604d88b4c59ae3f
hung <hunglt.ee@gmail.com>
2023-05-18 15:53:15 +0700

    run gem omniauth-slack

--- Gemfile
+++ Gemfile
@@ -34,6 +34,10 @@ gem 'letter_opener'
 
 gem "net-http"
 
+gem 'figaro'
+
+gem 'omniauth-slack'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,20 @@
 class StaticPagesController < ApplicationController
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
 
   def home
   end
 
   def help
+  end
+
+  def slack
+    auth_hash = request.env['omniauth.auth']
+    # auth_hash.credentials.token
+    # auth_hash.extra.user_info # me??
+    # auth_hash.info
+    # auth_hash.extra.bot_info.bot_user_id #bot
+    debugger
+    render html: auth_hash.info.email
   end
 
   private
