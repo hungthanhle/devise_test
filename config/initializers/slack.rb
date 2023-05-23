1590fedbe1b964d67875586b33bb825efa8e2dfc
hung <hunglt.ee@gmail.com>
2023-05-23 11:25:59 +0700

    client another_user_info test

--- config/initializers/slack.rb
+++ config/initializers/slack.rb
@@ -4,5 +4,7 @@ end
 
 # rails c
 # client = Slack::Web::Client.new
-# client.auth_test
+# client.auth_test <- me or bot <- 2 type (user and bot token) of SLACK_API_TOKEN
 # client.chat_postMessage(channel: '#general', text: 'https://api.slack.com/methods/auth.test/test', as_user: true)
+# client.users_info(user: 'U05854MSNLT').user.name #me
+# client.users_info(user: '@hunglt.ee').user.id #me
