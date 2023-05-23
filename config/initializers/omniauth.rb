f9db0ad6874510d7d6119e1f16df404b0cb67aae
hung <hunglt.ee@gmail.com>
2023-05-23 10:58:04 +0700

    Use slack API to get auth_user's info

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  # provider :slack, ENV['SLACK_APP_ID'], ENV['SLACK_APP_SECRET'], name: :sign_in_with_slack, scope: 'identity.basic'
   provider :slack, ENV['SLACK_APP_ID'], ENV['SLACK_APP_SECRET'], scope: 'team:read,users:read,identify,bot'
+  # provider :slack, ENV['SLACK_APP_ID'], ENV['SLACK_APP_SECRET'], :user_scope => 'users:read,identify,bot'
 end
