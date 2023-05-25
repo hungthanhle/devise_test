bd3aef2cc5816c9a8dbccfd9203459e4ec2a6fe8
hung <hunglt.ee@gmail.com>
2023-05-25 10:28:38 +0700

    think about it

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,27 +1,11 @@
 class SessionsController < Devise::SessionsController
   # https://github.com/heartcombo/devise/blob/main/app/controllers/devise/sessions_controller.rb
   def slack
-    # debugger
-    # auth_hash = request.env['omniauth.auth'] <- định vọc biến request
-    # token = auth_hash.credentials.token
-
-    # body = '{"client_id": ' + "#{ENV['SLACK_CLIENT_DEF_ID']} ," + '"client_secret":' + "#{ENV['SLACK_CLIENT_DEF_SECRET']}," + '"code":' + "#{params[:code]}" + '}' <- this is string
-    # body = {
-    #   client_id: ENV['SLACK_CLIENT_DEF_ID'],
-    #   client_secret: ENV['SLACK_CLIENT_DEF_SECRET'],
-    #   code: params[:code]
-    # }
-    # body = body.to_json
-    # response = Faraday.post('https://slack.com/api/oauth.v2.access', body, "Content-Type" => "application/json; charset=utf-8")
-    
-    # response = Faraday.get("https://slack.com/api/oauth.v2.exchange?client_id=#{ENV['SLACK_CLIENT_DEF_ID']}&client_secret=#{ENV['SLACK_CLIENT_DEF_SECRET']}&token=#{ENV['SLACK_API_DEF_TOKEN']}")
     response = Faraday.get("https://slack.com/api/oauth.v2.access?client_id=#{ENV['SLACK_CLIENT_DEF_ID']}&client_secret=#{ENV['SLACK_CLIENT_DEF_SECRET']}&code=#{params[:code]}")
-    
+    # -> authed_user (hunglt.ee) show info id <- no use me-token
     result = JSON.parse(response.body)
-    # ok = result["ok"]
-
-    # debugger
     incoming_webhook = result["incoming_webhook"]["url"]
-    render json: {slack: result.to_s, incoming_webhook: incoming_webhook}
+    render json: {slack: result.to_s, incoming_webhook: incoming_webhook} #
+    # think redirect
   end
 end
