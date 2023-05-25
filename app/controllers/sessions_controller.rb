70b1e462029e8763a5889e73745126c0fd89e747
hung <hunglt.ee@gmail.com>
2023-05-25 11:03:16 +0700

    think about it1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,8 @@ class SessionsController < Devise::SessionsController
     response = Faraday.get("https://slack.com/api/oauth.v2.access?client_id=#{ENV['SLACK_CLIENT_DEF_ID']}&client_secret=#{ENV['SLACK_CLIENT_DEF_SECRET']}&code=#{params[:code]}")
     # -> authed_user (hunglt.ee) show info id <- no use me-token
     result = JSON.parse(response.body)
-    incoming_webhook = result["incoming_webhook"]["url"]
+    # result["expires_in"]
+    incoming_webhook = result["incoming_webhook"]["url"] # EXPIRE OR NOT ??? <- webhooks only show once when slick install
     render json: {slack: result.to_s, incoming_webhook: incoming_webhook} #
     # think redirect
   end
